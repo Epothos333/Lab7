d9854ba3b95cf95aaaded0311857ca3bab943eb3
Edwin A <ea976@nyu.edu>
2016-02-04 12:50:45 -0500

    JS assignment

---
+++ Lab7.js
@@ -0,0 +1,13 @@
+var grocList = {};
+grocList.name = ["potatoes", "bananas", "hamburgers", "apples", "dogs", "cats", "leopards"];
+grocList.price = [0.99, 0.79, 2.99, 0.50, 30.00, 29.00, 44.99];
+var totalPrice = 0
+
+
+for (var i=0; i<grocList.name.length; i++) {
+	console.log(grocList.name[i]);
+	console.log("$" +grocList.price[i]);
+	totalPrice += grocList.price[i]
+}
+
+console.log("The total Price is: " + totalPrice);
---
+++ Lab7_1.js
@@ -0,0 +1,25 @@
+var makeArr = function(x,y) {
+	var arr = [];
+
+	if (x > y) {
+		var low = y;
+		var high = x;
+	}
+	else {
+		var low = x;
+		var high = y;
+	}
+	for (var i = low; i <= high; i++) {
+			arr.push(i);
+
+	}
+	if (x === y) {
+		arr = "that's the same number foo!"
+	}
+	return arr
+}
+
+console.log(makeArr(-4,2));
+console.log(makeArr(2,-4));
+console.log(makeArr(55,55));
+console.log(makeArr(-9,-4));
